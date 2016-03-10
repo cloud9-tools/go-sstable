4a35641d52f749d4247c3f8e192faf8fe0935212
Donald King <chronos@chronos-tachyon.net>
2016-03-10 12:40:32 -0800

    Factor out fullReadAt.

--- load.go
+++ load.go
@@ -5,17 +5,24 @@ import (
 	"io"
 )
 
-func (t *SSTable) load() error {
-	var pos int64
-	data := make([]byte, 4+len(magic))
-	n, err := t.f.ReadAt(data, 0)
+func fullReadAt(f io.ReaderAt, data []byte, offset int64) error {
+	n, err := f.ReadAt(data, offset)
 	if err != nil {
 		return err
 	}
 	if n < len(data) {
 		return io.ErrUnexpectedEOF
 	}
-	pos += int64(n)
+	return nil
+}
+
+func (t *SSTable) load() error {
+	data := make([]byte, 4+len(magic))
+	if err := fullReadAt(t.f, data, 0); err != nil {
+		return err
+	}
+	pos := int64(len(data))
+
 	if string(data[0:len(magic)]) != magic {
 		return ErrBadFormat
 	}
@@ -23,25 +30,17 @@ func (t *SSTable) load() error {
 	r := make([]record, count)
 	for i := uint32(0); i < count; i++ {
 		data = make([]byte, 9)
-		n, err = t.f.ReadAt(data, pos)
-		if err != nil {
+		if err := fullReadAt(t.f, data, pos); err != nil {
 			return err
 		}
-		if n < len(data) {
-			return io.ErrUnexpectedEOF
-		}
-		pos += int64(n)
+		pos += int64(len(data))
 		r[i].length = binary.BigEndian.Uint32(data[0:4])
 		r[i].offset = binary.BigEndian.Uint32(data[4:8])
 		key := make([]byte, data[8])
-		n, err = t.f.ReadAt(key, pos)
-		if err != nil {
+		if err := fullReadAt(t.f, key, pos); err != nil {
 			return err
 		}
-		if n < len(key) {
-			return io.ErrUnexpectedEOF
-		}
-		pos += int64(n)
+		pos += int64(len(key))
 		r[i].key = string(key)
 	}
 	t.r = r
